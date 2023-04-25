1c873ef198cb43830e9a9e12f234f72eda237f8e
Reezywise <reezybiyi@gmail.com>
2023-04-25 12:48:10 +0100

    router1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './components/navbar/Nav'
+import Footer from './components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/page1/Page1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from '../../components/navbar/Nav'
+import Footer from '../../components/Footer/Footer'
+
+const Page1 = () => {
+  return (
+    <div>
+        <Nav/>
+        <Footer/>
+      
+    </div>
+  )
+}
+
+export default Page1
